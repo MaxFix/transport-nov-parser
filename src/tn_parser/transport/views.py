facdcaf4e689c6e720c2eea1e871cb753a6c9ad2
xmx <max.sox@mail.ru>
2017-08-09 23:20:20 +0300

    number of marshrt on the schedule page

--- src/tn_parser/transport/views.py
+++ src/tn_parser/transport/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Route, RouteTypes, RoutePoint
+from .models import Route, RouteTypes, RoutePoint, EnumBase
 
 
 def main(request):
@@ -12,7 +12,7 @@ def schedule(request):
     route = Route.objects.filter(code=request.GET['mar']).first()
     stops = RoutePoint.objects.filter(route=route)
 
-    return render(request, 'schedule.html', {'stops': stops})
+    return render(request, 'schedule.html', {'stops': stops, 'type_m': route.type, 'mar_n': route.name, 'bus' : RouteTypes.BUS, 'trolleybus' : RouteTypes.TROLLEYBUS})
 
 def bus_schedule(request):
     return render(request, 'bus_schedule.html', {})
