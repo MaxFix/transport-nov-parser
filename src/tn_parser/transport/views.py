6a1d4120abf74cc09e637fcbe257be6466b96035
xmx <max.sox@mail.ru>
2017-09-03 21:16:49 +0300

    Get parameter complete

--- src/tn_parser/transport/views.py
+++ src/tn_parser/transport/views.py
@@ -1,5 +1,7 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from .models import Route, RouteTypes, RoutePoint, Stop
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def main(request):
@@ -9,13 +11,27 @@ def main(request):
     return render(request, 'index.html', {'buses': buses, 'trolleybuses': trolleybuses})
 
 def schedule(request):
-    route = Route.objects.filter(code=request.GET['mar']).first()
-    stops = RoutePoint.objects.filter(route=route)
+    # if not len(request.GET):            Проверка заполненности
+    #     return HttpResponse('non')
+    try:
+        route = Route.objects.get(code=request.GET['mar'])
+        stops = RoutePoint.objects.filter(route=route)
+    except ObjectDoesNotExist:
+        raise Http404('Не найден маршрут')
+    except KeyError:
+        raise Http404('Не найден mar param')
 
-    return render(request, 'schedule.html', {'stops': stops, 'type_m': route.type, 'mar_n': route.name, 'bus': RouteTypes.BUS, 'trolleybus': RouteTypes.TROLLEYBUS})
+    return render(request, 'schedule.html', {'stops': stops, 'type_m': route.type, 'mar_n': route.name, 'bus': RouteTypes.BUS, 'trolleybus': RouteTypes.TROLLEYBUS, 'mar': request.GET['mar']})
+
+#добавить исключение некорректного и пустого запроса
+#корректное получение get запроса
 
 def bus_schedule(request):
-    schedules = Stop.objects.filter(id=request.GET['stp']).first()
-    mar_bs = Route.objects.filter(code=Route.code)
+    #через query set GET получить остановку (вместо фильтар)
+    #получить из ... маршрут
+    #получить время из роут поин через фильтр, отформатировать и отобразить (список с одним элементом)
+    stop = Stop.objects.get(id=request.GET['stp']) # неверно, Stop не итерируемый объект, несостыковка типов?
+    mar_bs = Route.objects.filter(code=request.GET['mar'])
+    mar_time = RoutePoint.objects.filter(route= mar_bs,stop= stop) # похоже на бред
 
-    return render(request, 'bus_schedule.html', {'schedules': schedules, 'mar_bs': mar_bs})
+    return render(request, 'bus_schedule.html', {'schedules': mar_time, 'mar_bs': mar_bs, 'mar_time': mar_time})
