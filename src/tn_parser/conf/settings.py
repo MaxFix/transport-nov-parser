358853e9f2142557177ef38d97763ee4ba696406
xmx <max.sox@mail.ru>
2017-10-06 01:04:09 +0300

    - changed the time format - made the switch route directions

--- src/tn_parser/conf/settings.py
+++ src/tn_parser/conf/settings.py
@@ -159,7 +159,13 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
+
+LANGUAGE_CODE = 'ru-RU'
+
+# Пример вывода: 16 сентября 2012
+TIME_FORMAT = 'H:i'
+
 
 USE_TZ = True
 
